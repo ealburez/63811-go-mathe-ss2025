126ed21e6507e58eea086d5fbf19d8c62a27b46a
ealburez <ealburez@gmail.com>
2025-04-04 08:10:55 +0200

    test for main

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	s := "gopher"
 	fmt.Println("Hello and welcome, %s!", s)
 
-	for i := 1; i <= 4; i++ {
+	for i := 1; i <= 7; i++ {
 		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
 		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
 		fmt.Println("i =", 100/i)
