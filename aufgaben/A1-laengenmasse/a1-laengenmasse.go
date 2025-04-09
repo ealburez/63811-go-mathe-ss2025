16672663bd742cb43c5bbc05f319a68b7fd172d2
ealburez <61049785+ealburez@users.noreply.github.com>
2025-04-09 12:49:02 +0000

    1st working solution

---
+++ aufgaben/A1-laengenmasse/a1-laengenmasse.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	const inch_cr float64 = 2.54
+	const mm_cr float64 = 0.001
+	const km_cr float64 = 1000
+	const seamiles_cr float64 = 1852
+	const lightyear_cr float64 = 9_460_730_472_580_800
+	var input_in_meter float64
+
+	fmt.Println("Enter the length in meter:")
+	fmt.Scan(&input_in_meter)
+	fmt.Printf("\n%.2e mm", input_in_meter / mm_cr)
+	fmt.Printf("\n%8.2f km", input_in_meter / km_cr)
+	fmt.Printf("\n%.2e in", input_in_meter / inch_cr)
+	fmt.Printf("\n%8.2f Sea Miles", input_in_meter / seamiles_cr)
+	fmt.Printf("\n%.2e Light Years:", input_in_meter / lightyear_cr)
+
+
+
+}
