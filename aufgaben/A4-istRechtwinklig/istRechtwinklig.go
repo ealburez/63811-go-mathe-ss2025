99e17d87624e1f8f20c42e42838ddb422093130b
ealburez <61049785+ealburez@users.noreply.github.com>
2025-04-11 13:42:00 +0000

    Finished Aufgabe 4

--- aufgaben/A4-istRechtwinklig/istRechtwinklig.go
+++ aufgaben/A4-istRechtwinklig/istRechtwinklig.go
@@ -3,6 +3,21 @@ package main
 // istRechtwinklig prüft, ob das durch die Seitenlängen x, y, z gebildete
 // Dreieck rechtwinklig ist.
 func istRechtwinklig(x, y, z int) bool {
-	// bitte korrigieren Sie den Rumpf dieser Funktion.
-	return true
+	
+	if x > y && x > z{
+		return pythagorasCheck(x,y,z)
+	} else if y > x && y > z {
+		return pythagorasCheck(y,x,z)
+	}else{
+		return pythagorasCheck(z,y,x)
+	}
+
+}
+
+func pythagorasCheck(a,b,c int) bool {
+	result:= false
+	if (a*a) == (b*b + c*c) {
+		result = true
+	}
+	return result
 }
