e749124e41b0e83fca635b3030d747b95291054f
ealburez <61049785+ealburez@users.noreply.github.com>
2025-05-06 13:21:04 +0000

    var_and_constants done

---
+++ excercises-bergamn/1_hello_world/variables_and_constants.go
@@ -0,0 +1,13 @@
+//https://github.com/bregman-arie/go-exercises/blob/main/exercises/variables/exercise.md
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	var myName = "enio"
+	const myAge = 24 
+
+	fmt.Printf("My name is %v and my age is %v \n", myName, myAge)
+}
