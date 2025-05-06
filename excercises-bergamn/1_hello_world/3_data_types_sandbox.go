46ff4f8a71cc657d0455e7c310ba581f9ccaa2be
ealburez <61049785+ealburez@users.noreply.github.com>
2025-05-06 13:51:47 +0000

    3_data_types done

---
+++ excercises-bergamn/1_hello_world/3_data_types_sandbox.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var food = "Pizza"
+    var slices = 4
+    var pineappleOnPizza = true
+
+	fmt.Printf("%T %T %T", food,slices,pineappleOnPizza)
+}
